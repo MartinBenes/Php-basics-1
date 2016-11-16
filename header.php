0b3d1cf713798ee6bfb9eb4496b8e34fa5f56f43
MartinBenes <martin.beness@gmail.com>
2016-11-16 14:47:42 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Kraje ČR</title>
+    <style>
+        aside {width: 33%; float: left; border: 1px solid gray}
+        </style>
+</head>
+<body>
